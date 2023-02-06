e54117be7ed76aea43abce3d56650690b0204a46
hwatso02 <hwatso02@gmail.com>
2023-02-06 10:43:45 -0700

    Part way through Views

--- Mission04_hwatso02/Startup.cs
+++ Mission04_hwatso02/Startup.cs
@@ -16,13 +16,28 @@ namespace Mission04_hwatso02
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            //For checking errors
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Create endpoints and not use default files bc MVC
             app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "(controller=cshtml) /{action = Index}/{id?}"
+                    );
+            });
         }
     }
 }
